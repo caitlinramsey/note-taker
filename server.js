10214eb86734809d026ec10a8914e651f8e65cc0
Caitlin <caitlinramsey@outlook.com>
2023-05-27 13:39:47 -0400

    added gitignore and added to package.json

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/notes', (req, res) {
+app.get('/api/notes', (req, res) => {
     try {
         noteData = fs.readFileSync('db/db.json', 'utf8');
         console.log('Hello from the app!');
